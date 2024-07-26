0b670a341f68c63ba9b08423f9c6ceba84e2bd87
frarosset <37275581+frarosset@users.noreply.github.com>
2024-07-27 01:01:34 +0200

    Add projectFormDomComponent and todoFormDomComponent classes
    These extend the baseFormDomComponent class. As of now, they just
    change the title of the dialog and the classes names.

---
+++ src/js-modules/projectFormDomComponent.js
@@ -0,0 +1,12 @@
+import baseFormDomComponent from "./baseFormDomComponent.js";
+
+export default class projectFormDomComponent extends baseFormDomComponent {
+  static blockName = "project-form-dialog";
+  static type = "Project";
+
+  constructor(obj) {
+    super(obj);
+  }
+
+  // Methods
+}
---
+++ src/js-modules/todoFormDomComponent.js
@@ -0,0 +1,12 @@
+import baseFormDomComponent from "./baseFormDomComponent.js";
+
+export default class todoFormDomComponent extends baseFormDomComponent {
+  static blockName = "todo-form-dialog";
+  static type = "Todo";
+
+  constructor(obj) {
+    super(obj);
+  }
+
+  // Methods
+}
