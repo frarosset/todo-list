6a85ad3ac95cde810a935e57fe5e9e0b5c8dcf82
frarosset <37275581+frarosset@users.noreply.github.com>
2024-08-06 23:53:05 +0200

    Load root data from localStorage or sampleData.json at page load
    First, the program checks for the data in the localStorage. If
    this is present, it loads it. Otherwise, it loads sample data from
    the sampleData.json. Note that to load a page with no data,
    sampleData.json must contain '{}'.

--- src/js-modules/initWebpage.js
+++ src/js-modules/initWebpage.js
@@ -1,25 +1,47 @@
 import rootComponent from "./logic/rootComponent.js";
 import domStructure from "./dom/domStructure.js";
-import sampleDataJSON from "../sampleData.json";
+import sampleDataJSONObj from "../sampleData.json";
 import PubSub from "pubsub-js";
 
 export default function initWebpage() {
-  // some sample data -> todo: read from localstorage or some defaultData.json
-  // Stringify the sampleDataJSON to match the input required by rootComponent (it will be parsed with a reviver in there)
-  const rootDataJSON = JSON.stringify(sampleDataJSON);
+  const root = new rootComponent(getRootDataJSON());
 
-  const root = new rootComponent(rootDataJSON);
-
-  console.log(root.print());
-
-  // subscribe to EDITED token
-  PubSub.subscribe("EDITED -> saving into localStorage", (msg) => {
-    console.log(msg);
-    // Get a JSON representation of the data and save it to local storage
-    const currentRootDataJSON = JSON.stringify(root);
-    localStorage.setItem("rootDataJSON", currentRootDataJSON);
-  });
+  subscribeToSetRootDataJSON(root);
 
   //Build the dom structure
   new domStructure(root);
+}
+
+function getRootDataJSON() {
+  // try to load from localStorage
+  const localStorageDataJSON = localStorage.getItem("rootDataJSON");
+  if (localStorageDataJSON) {
+    return localStorageDataJSON;
+  }
+
+  // load some sample data from sampleData.json
+  // Stringify the sampleDataJSON to match the input required by rootComponent (it will be parsed with a reviver in there)
+  const sampleDataJSON = JSON.stringify(sampleDataJSONObj);
+  if (sampleDataJSON !== "{}") {
+    return sampleDataJSON;
+  } else {
+    // if empty ({}), return null
+    return null;
+  }
+}
+
+function subscribeToSetRootDataJSON(root) {
+  const saveData = (root) => {
+    // Get a JSON representation of the data and save it to local storage
+    const currentRootDataJSON = JSON.stringify(root);
+    localStorage.setItem("rootDataJSON", currentRootDataJSON);
+  };
+
+  saveData(root);
+
+  // subscribe to EDITED token
+  PubSub.subscribe("EDITED", (msg) => {
+    console.log(`${msg} -> saving into localStorage`);
+    saveData(root);
+  });
 }
